bf99b64da256aaa18d5605ede154bd27fbb03ec1
Michael Noonan <michaelnoonan@Michaels-MacBook-Pro-2.local>
2019-06-19 13:20:44 -0400

    Some minor changes to the UI

--- App.R
+++ App.R
@@ -1,50 +1,61 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 
-
 library(shiny)
 
 source("Correction_Calculator.R")
 
 ui <- fluidPage(
-  headerPanel("Calculate a mass specific home range area correction"),
+  titlePanel("HomeRange Correction"),
   
-  sidebarPanel(width = 4,
+  sidebarLayout(
+    sidebarPanel(
+      
+      helpText("Calculate a mass specific correction for a home range area estimated from a conventional estimator."),
+      
+      width = 5,
+      
+      numericInput("area","Home range (area)", NULL, min = 0, max = Inf),
+      
+      numericInput("mass","Mass (kg)", NULL, min = 0, max = Inf),
+      
+      selectInput("Estimator", 
+                  label = "Original estimator (Not currently functional)",
+                  choices = c("KDE - Gaussian reference function", 
+                              "KDE - Silverman's rule of thumb",
+                              "KDE - Least Squares Cross-Validation", 
+                              "MCP"),
+                  selected = "KDE - Gaussian reference function"),
+      
+      actionButton("calculate", "Calculate")
+      
+    ),
     
-    numericInput("area","Home range area", NULL, min = 0, max = Inf),
-    
-    numericInput("mass","Mass", NULL, min = 0, max = Inf),
-    
-    selectInput("Estimator", 
-                label = "Original estimator",
-                choices = c("KDE - Gaussian reference function", 
-                            "KDE - Silverman's rule of thumb",
-                            "KDE - Least Squares Cross-Validation", 
-                            "MCP"),
-                selected = "KDE - Gaussian reference function"),
-    
-    actionButton("add", "Calculate")
-    
-  ),
-  
-  mainPanel(
-    
-    tableOutput("results")
-    
+    mainPanel(width = 6,
+              headerPanel("Corrected area"),
+              
+              tableOutput("results")
+              
+    )
   )
-)
+  
+)#Closes the ui
+
+
 
 server <- function(input, output){
   
   output$results <- renderTable(HR_Correction(Area =0, Mass = 0))
   
-  observeEvent( input$add,{
-  output$results <- renderTable(HR_Correction(Area = input$area, Mass = input$mass))
+  
+  
+  observeEvent( input$calculate,{
+    output$results <- renderTable(HR_Correction(Area = input$area, Mass = input$mass))
   }
   
   )
   
-
+  
   
   #output$results <- renderTable({RES})
 }
