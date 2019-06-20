0fd7c067aed1148d0f0d2d70de7ce010e4552a85
Michael Noonan <michaelnoonan@Michaels-MacBook-Pro-2.local>
2019-06-20 10:53:44 -0400

    Switch to Dashboard UI

---
+++ R/HomeRangeCorrection.R
@@ -0,0 +1,14 @@
+#' @title HomeRangeCorrection
+#'
+#' @description A shiny app for correcting home range areas for mass specific bias.
+#'
+#' HomeRangeCorrection: A shiny app package for correction range estimates
+#'
+#'
+#' - [github repo](https://github.com/NoonanM/HomeRange_Correction.git)
+#'
+#' @docType package
+#' @name HomeRangeCorrection
+#' @import shiny
+#' @import shinydashboard
+NULL
